6399154a9ab80340458ba6ad0001d18db7a84303
wibrst <wibrst@qq.com>
2020-03-25 13:33:19 +0800

    fix download error

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -1,5 +1,6 @@
 _get_latest_version() {
-	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+	v2ray_repos_url="https://api.github.com/repos/v2ray/v2ray-core/releases/latest?v=$RANDOM"
+	v2ray_latest_ver="$(curl -s $v2ray_repos_url | grep 'tag_name' | cut -d\" -f4)"
 
 	if [[ ! $v2ray_latest_ver ]]; then
 		echo
@@ -14,10 +15,8 @@ _get_latest_version() {
 }
 
 _download_v2ray_file() {
-	_get_latest_version
-	[[ -d /tmp/v2ray ]] && rm -rf /tmp/v2ray
-	mkdir -p /tmp/v2ray
-	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
+	[[ ! $v2ray_latest_ver ]] && _get_latest_version
+	v2ray_tmp_file="/tmp/v2ray.zip"
 	v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
 
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
@@ -26,26 +25,17 @@ _download_v2ray_file() {
         " && exit 1
 	fi
 
-	unzip $v2ray_tmp_file -d "/tmp/v2ray/"
-	mkdir -p /usr/bin/v2ray
-	cp -f "/tmp/v2ray/v2ray" "/usr/bin/v2ray/v2ray"
-	chmod +x "/usr/bin/v2ray/v2ray"
-	echo "alias v2ray=$_v2ray_sh" >>/root/.bashrc
-	cp -f "/tmp/v2ray/v2ctl" "/usr/bin/v2ray/v2ctl"
-	chmod +x "/usr/bin/v2ray/v2ctl"
+	unzip -o $v2ray_tmp_file -d "/usr/bin/v2ray/"
+	chmod +x /usr/bin/v2ray/{v2ray,v2ctl}
+	if [[ ! $(cat /root/.bashrc | grep v2ray) ]]; then
+		echo "alias v2ray=$_v2ray_sh" >>/root/.bashrc
+	fi
 }
 
 _install_v2ray_service() {
-	if [[ $systemd ]]; then
-		cp -f "/tmp/v2ray/systemd/v2ray.service" "/lib/systemd/system/"
-		sed -i "s/on-failure/always/" /lib/systemd/system/v2ray.service
-		systemctl enable v2ray
-	else
-		apt-get install -y daemon
-		cp "/tmp/v2ray/systemv/v2ray" "/etc/init.d/v2ray"
-		chmod +x "/etc/init.d/v2ray"
-		update-rc.d -f v2ray defaults
-	fi
+	cp -f "/usr/bin/v2ray/systemd/v2ray.service" "/lib/systemd/system/"
+	sed -i "s/on-failure/always/" /lib/systemd/system/v2ray.service
+	systemctl enable v2ray
 }
 
 _update_v2ray_version() {
